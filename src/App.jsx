e68f52cd483ee782e140cc907beec5be5d847f08
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-30 19:01:35 +0530

    basic wire frame making done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,44 @@ import './App.css'
 
 function App() {
 
+  const checkboxData = [
+    {title : "Include Uppercase Letters", state: false},
+    {title : "Include Lowercase Letters", state : false},
+    {title : "Include Numbers", state : false},
+    {title : "Include Symbols", state : false}
+  ]
+
   return (
-  <div>
-    <h1>Password Generator</h1>
-  </div>
+    <div >
+      <h1>Password Generator</h1>
+      <div className='container'>
+        <div className='header'>
+          <div className="title">dafbakjfj</div>
+          <button className='copyBtn' onClick={() => { }}>Copy</button>
+        </div>
+        <div className='charLength'>
+          <span>
+            <label >Charcter Length</label>
+            <label >4</label>
+          </span>
+          <input type="range" min={4} max={20} value={13} />
+
+        </div>
+        <div className='checkboxes'>
+          {
+            checkboxData.map((checkbox , index)=>{
+              return <div key={index}>
+                <input type="checkbox" checked={checkbox.state} />
+                <label >{checkbox.title}</label>
+                 </div>
+            })
+          }
+        </div>
+
+        <button className='generateBtn'>Generate Button</button>
+
+      </div>
+    </div>
   )
 }
 
